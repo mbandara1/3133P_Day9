151fb4417b7e9650d6c450529a96a92f42858d15
Madhuranga Bandara <madurangabandara2346@gmail.com>
2024-12-22 18:54:45 +0000

    modified index app

--- my_app/app/(tabs)/index.tsx
+++ my_app/app/(tabs)/index.tsx
@@ -1,26 +1,16 @@
-import { Text, View,Image,StyleSheet } from "react-native";
+import { Text, View , Image} from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image source={require("../assets/images/react-logo.png")} style={styles.logo}></Image>
-      <Text>Hello World!</Text>
-      <Text>My First React Native cross platform app</Text>
-    </View>
-  );
+return (
+<View
+style={{
+flex: 1,
+justifyContent: "center",
+alignItems: "center",
+}}
+>
+      <Image source={require('../assets/images/react-logo.png')}/>
+<Text>Hello world</Text>
+</View>
+);
 }
-
-const styles=StyleSheet.create({
-  logo:{
-  width:100,
-  height:100,
-  borderColor:'solid',
-  borderWidth:2
-  }
-})
