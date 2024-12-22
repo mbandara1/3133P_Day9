1757ee551dada210591e861cefcb2adb319c1e59
Madhuranga Bandara <madurangabandara2346@gmail.com>
2024-12-22 19:07:27 +0000

    styles added

--- my_app/app/(tabs)/index.tsx
+++ my_app/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View , Image} from "react-native";
+import { Text, View , Image, StyleSheet} from "react-native";
 
 export default function Index() {
 return (
@@ -9,9 +9,22 @@ justifyContent: "center",
 alignItems: "center",
 }}
 >
-<Text>Hello world</Text>
-<Image source={require('../../assets/images/react-logo.png')}/>
+<Text style={styles.text}>Hello world</Text>
+<Image source={require('../../assets/images/react-logo.png')}style={styles.logo}/>
 
 </View>
 );
 }
+const styles = StyleSheet.create({
+  logo:{
+    width:100,
+    height:100,
+    borderStyle:"solid",
+    borderWidth:2,
+    borderColor:"blue"
+  },
+  text:{
+      color:"red",
+      fontSize:36
+  }
+})
